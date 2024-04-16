be643cba5153678dcd2d6067292e688f835eb370
Mundane <mundanepeasant@gmail.com>
2024-04-15 22:07:34 -0500

    load all base stats

--- src/Card.jsx
+++ src/Card.jsx
@@ -11,14 +11,20 @@ function Card({ pokemon }) {
       <div className="row-one">
         <img src={imageURL} alt={name} />
         <div className="card-sub-info">
-          <p>{weight}</p>
           <p>{hp}</p>
           <p>{att}</p>
           <p>{def}</p>
+          <p>{specAtt}</p>
+          <p>{specDef}</p>
+          <p>{speed}</p>
         </div>
       </div>
-      <h2>{name.toUpperCase()}</h2>
       <div className="row-two">
+        <h2>{name.toUpperCase()}</h2>
+        <h3>{baseStats + weight}</h3>
+      </div>
+
+      <div className="row-three">
         <div className="increment-decrement">
           <div>+</div>
           <div>#</div>
